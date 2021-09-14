94cffb8bdc76cbb2e9dd917aff99f8339c9b75c6
Lucas Lopes <68665689+lucaslopesx@users.noreply.github.com>
2021-09-14 14:13:59 -0300

    crud operations OK

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
-'use strict'
+"use strict";
 
-const openModal = () => document.getElementById('modal')
-    .classList.add('active')
+const openModal = () =>
+    document.getElementById("modal").classList.add("active");
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
+const closeModal = () =>
+    document.getElementById("modal").classList.remove("active");
 
-document.getElementById('cadastrarCliente')
-    .addEventListener('click', openModal)
+const tempClients = {
+    name: "Lucas",
+    email: "lucas.lopesx1@gmail.com",
+    cellphone: "35999694467",
+    city: "Machado",
+};
 
-document.getElementById('modalClose')
-    .addEventListener('click', closeModal)
+const getLocalStorage = () =>
+    JSON.parse(localStorage.getItem("db_client")) ?? [];
+const setLocalStorage = (dbClient) =>
+    localStorage.setItem("db_client", JSON.stringify(dbClient));
+
+//CRUD - create read update delete button
+const createClient = (client) => {
+    const dbClient = getLocalStorage();
+    dbClient.push(client);
+    setLocalStorage(dbClient);
+};
+
+const readClient = () => getLocalStorage();
+
+const updateClient = (index, client) => {
+    const dbClient = readClient();
+    dbClient[index] = client;
+    setLocalStorage(dbClient);
+};
+
+const deleteClient = (index) => {
+    const dbClient = readClient();
+    dbClient.splice(index, 1);
+    setLocalStorage(dbClient);
+};
+
+document
+    .getElementById("cadastrarCliente")
+    .addEventListener("click", openModal);
+
+document.getElementById("modalClose").addEventListener("click", closeModal);
